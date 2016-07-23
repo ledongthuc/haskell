e693f8a4a60d1ca71cea4929568e7fae7ddbab08
Thuc Le <ledongthuc@gmail.com>
2016-07-23 18:04:18 +0700

    Add boolean examples

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Save example that we are done. You can search by Table Index.
 # TABLE OF CONTENT 
 
  - [Hello world](example/hello_world.hs)
- - [Simple mathermatics](example/simple_mathermatics.hs)
+ - [Simple operators](example/simple_operator.hs)
+ - [Boolean](example/boolean.hs)
---
+++ examples/boolean.hs
@@ -0,0 +1,14 @@
+result :: Bool
+
+-- result = True && False
+-- result = True && True
+-- result = True || False
+-- result = True || True
+-- result = not False
+-- result = not (True && True)
+-- result = 5 == 5
+-- result = 1 == 0
+-- result = 5 /= 5
+result = 1 /= 0
+
+main = print(result)
