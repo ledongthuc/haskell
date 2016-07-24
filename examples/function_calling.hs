6182b631d26254a2d240417bccb6c7881ac39eb2
Thuc Le <ledongthuc@gmail.com>
2016-07-24 10:36:05 +0700

    Add more function calling with max and min

--- examples/function_calling.hs
+++ examples/function_calling.hs
@@ -1,9 +1,11 @@
 x :: Integer
 y :: Integer
 z :: Integer
+w :: Integer
 
 x = 5 + 7 -- infix function
 y = x * 2
 z = succ y -- prefix function
+w = min 10 5 + max 5 10
 
-main = print(z)
+main = print(z `div` 2)
