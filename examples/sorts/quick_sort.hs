a2e7217ae12d749c0c9a011e25d38ebf741cfed4
Thuc Le <ledongthuc@gmail.com>
2016-08-25 15:30:11 +0700

    Improve quick sort

--- examples/sorts/quick_sort.hs
+++ examples/sorts/quick_sort.hs
@@ -1,7 +1,7 @@
 quickSort::(Ord a)=>[a]->[a]
 quickSort []     = []
 quickSort (x:xs) = (quickSort lessList) ++ [x] ++ (quickSort greaterList)
-                 where lessList = [z | z<-xs, z <= x]
-                       greaterList = [z | z<-xs, z > x]
+                 where lessList = filter (<= x) xs
+                       greaterList = filter (> x) xs
 
 main = print(quickSort [1,5,7,9,0,2,3,4,6,3,1,4,7,7,3,2,45,6,87])
