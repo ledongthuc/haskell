e7f4c0cc88ec52c41a4ecb9e574ca5e9250e4c8d
Thuc Le <ledongthuc@gmail.com>
2016-09-15 22:31:49 +0700

    Update flip

--- examples/common_functions/reverse.hs
+++ examples/common_functions/reverse.hs
@@ -5,6 +5,9 @@ reverse' (x:xs) = (reverse' xs) ++ [x]
 reverse2'::[a]->[a]
 reverse2' xs = foldl (\x y -> y : x) [] xs
 
+reverse3'::[a]->[a]
+reverse3' xs = foldl ( flip (:) ) [] xs
+
 main = do
   putStrLn( show( reverse' [1,2,3,4,5] ))
   putStrLn( show( reverse' [1..10] ))
@@ -12,3 +15,6 @@ main = do
   putStrLn( show( reverse2' [1,2,3,4,5] ))
   putStrLn( show( reverse2' [1..10] ))
   putStrLn( show( reverse2' []::[Double] ))
+  putStrLn( show( reverse3' [1,2,3,4,5] ))
+  putStrLn( show( reverse3' [1..10] ))
+  putStrLn( show( reverse3' []::[Double] ))
