630b88ea38bb5d9c91a4d5aa8286aa27ced8d0b5
Thuc Le <ledongthuc@gmail.com>
2016-09-27 07:15:55 +0700

    Define data type

--- src/define_data_type.hs
+++ src/define_data_type.hs
@@ -6,7 +6,7 @@ data Shape = Circle Point Float | Rectangle Point Point
 
 data Car = Car { company :: String
                  , model :: String
-                 , year :: Int } deriving(Show)
+                 , year :: Int } deriving(Eq, Show, Read)
 
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
@@ -23,4 +23,18 @@ main = do
   putStrLn $ show $ area $ Rectangle (Point 5.0 10.0) (Point 15.0 20.0) -- 100
   putStrLn $ show $ nudge (Circle (Point 10.0 20.0) 10.0) 10.0 10.0 -- Circle (Point 20.0 30.0) 10.0
   putStrLn $ show $ nudge (Rectangle (Point 5.0 10.0) (Point 15.0 20.0)) 10.0 10.0 --Rectangle (Point 15.0 20.0) (Point 25.0 30.0)
+
+  -- Usage of Eq derived
+  let ford = Car { company="Ford", model="Mustang", year=1967}
+      honda = Car { company="Honda", model="A123", year=1990} 
+      listOfCar = [ford, honda]
+  putStrLn $ show $ ford == honda -- false
+  putStrLn $ show $ ford /= honda -- true
+  putStrLn $ show $ ford `elem` listOfCar -- true
+
+  -- Usage of Show derived
   putStrLn $ show $ Car { company="Ford", model="Mustang", year=1967}
+
+  -- Usage of Read derived
+  putStrLn $ show $ read "Car { company=\"Ford\", model=\"Mustang\", year=1967}" == Car { company="Ford", model="Mustang", year=1967} -- True
+  
