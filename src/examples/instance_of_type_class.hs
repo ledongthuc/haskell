112ac8011856f7e71f5a80bb2858cba8a9f220d4
Thuc Le <ledongthuc@gmail.com>
2016-11-15 14:55:22 +0700

    Custom type class

--- src/examples/instance_of_type_class.hs
+++ src/examples/instance_of_type_class.hs
@@ -6,7 +6,24 @@ instance Eq TrafficLight where
   Yellow == Yellow = True
   _ == _ = False
 
+instance Show TrafficLight where
+  show Red = "Light is RED"
+  show Green = "Light is GREEN"
+  show Yellow = "Light is Yellow"
+
+class YesNo a where
+  yesno :: a -> Bool
+
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno Green = True
+  yesno Yellow = False
+
 main = do
   putStrLn $ show $ Red == Red
   putStrLn $ show $ Red == Yellow 
   putStrLn $ show $ Yellow == Yellow 
+  putStrLn $ show $ [Red, Green, Yellow]
+  putStrLn $ show $ yesno Red
+  putStrLn $ show $ yesno Green
+  putStrLn $ show $ yesno Yellow
