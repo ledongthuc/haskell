3c393bc89f83839e2ac02a632c352993ce587d1f
Thuc Le <ledongthuc@gmail.com>
2016-12-29 20:39:22 +0700

    correct problem 20

--- src/99-questions/problem-20.hs
+++ src/99-questions/problem-20.hs
@@ -5,22 +5,18 @@
  -    => ('b',"acd")
 |-} 
 
-removeAt :: Int -> [a] -> ([a], [a])
-removeAt no list  = (leftList, rightList)
-  where leftList  = [ value | (subNo, value) <- zip [1..] list, no == subNo ]
-        rightList = [ value | (subNo, value) <- zip [1..] list, no /= subNo ]
+removeAt :: Int -> [a] -> (a, [a])
+removeAt no list
+    | no <= 0 || no > length list = error "Index is not correct"
+    | no > 0  = (list!!(no-1), rightList)
+      where rightList = [ value | (subNo, value) <- zip [1..] list, no /= subNo ]
 
 main = do
   putStrLn $ "removeAt 1    ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 1    ['a', 'b', 'c', 'd'] )
   putStrLn $ "removeAt 2    ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 2    ['a', 'b', 'c', 'd'] )
   putStrLn $ "removeAt 4    ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 4    ['a', 'b', 'c', 'd'] )
-  putStrLn $ "removeAt 0    ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 0    ['a', 'b', 'c', 'd'] )
-  putStrLn $ "removeAt (-1) ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt (-1) ['a', 'b', 'c', 'd'] )
-  putStrLn $ "removeAt 10   ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 10   ['a', 'b', 'c', 'd'] )
-
-  putStrLn $ "removeAt 2    ([]::[Int]) : " ++  ( show $ removeAt 2    ([]::[Int]) )
-  putStrLn $ "removeAt 0    ([]::[Int]) : " ++  ( show $ removeAt 0    ([]::[Int]) )
-  putStrLn $ "removeAt (-1) ([]::[Int]) : " ++  ( show $ removeAt (-1) ([]::[Int]) )
-  putStrLn $ "removeAt 10   ([]::[Int]) : " ++  ( show $ removeAt 10   ([]::[Int]) )
+  -- putStrLn $ "removeAt 0    ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 0    ['a', 'b', 'c', 'd'] )
+  -- putStrLn $ "removeAt (-1) ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt (-1) ['a', 'b', 'c', 'd'] )
+  -- putStrLn $ "removeAt 10   ['a', 'b', 'c', 'd'] : " ++  ( show $ removeAt 10   ['a', 'b', 'c', 'd'] )
 
 -- More solutions: https://wiki.haskell.org/99_questions/Solutions/20
